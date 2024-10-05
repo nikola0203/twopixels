54451194bb4f2c921ab9266aaf68f408ff65bee1
Nikola <nikolahissar@gmail.com>
2024-10-05 14:53:08 +0200

    fix gitignore

---
+++ themes/twopixels-theme/assets/dist/js/admin.js
@@ -0,0 +1,26 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkadvanced_wp_starter_theme"] = self["webpackChunkadvanced_wp_starter_theme"] || []).push([["/js/admin"],{
+
+/***/ "./assets/src/scripts/admin.js":
+/*!*************************************!*\
+  !*** ./assets/src/scripts/admin.js ***!
+  \*************************************/
+/***/ (() => {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9hc3NldHMvc3JjL3NjcmlwdHMvYWRtaW4uanMiLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9hZHZhbmNlZC13cC1zdGFydGVyLXRoZW1lLy4vYXNzZXRzL3NyYy9zY3JpcHRzL2FkbWluLmpzPzVjZWUiXSwic291cmNlc0NvbnRlbnQiOlsiIl0sIm1hcHBpbmdzIjoiIiwiaWdub3JlTGlzdCI6W119\n//# sourceURL=webpack-internal:///./assets/src/scripts/admin.js\n");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/src/scripts/admin.js"));
+/******/ }
+]);
